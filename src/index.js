becd46d64aaac8973d0c56851c09d15eaa323581
Xvezda <xvezda@naver.com>
2022-01-24 08:41:58 +0900

    feat: 기본 mount 구현

--- src/index.js
+++ src/index.js
@@ -5,10 +5,38 @@ function createElement(type, props, ...children) {
   };
 }
 
+function mount(tree) {
+  if (typeof tree === 'string') {
+    return document.createTextNode(tree);
+  }
+  const { type, props } = tree;
+  const node = document.createElement(type);
+  if (props && props.children) {
+    props.children
+      .map(mount)
+      .forEach(node.appendChild.bind(node));
+  }
+  return node;
+}
+
+function render(element, container) {
+  container.appendChild(mount(element));
+}
+
 const h = createElement;
 
-  h('ul', null,
-    h('li', null, 'foo'),
-    h('li', null, 'bar'),
-  )
+const prevTree = h('ul', null,
+  h('li', null, 'foo'),
+  h('li', null, 'bar'),
+);
+
+const nextTree = h('ul', null,
+  h('li', null, 'foo'),
+  h('li', null, 'bar'),
+  h('li', null, 'baz'),
+);
+
+render(
+  prevTree,
+  document.body,
 );
