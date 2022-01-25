5598786d3278c960aa0dbc62d73e3639b0d2b737
Xvezda <xvezda@naver.com>
2022-01-25 16:38:18 +0900

    feat: 링크 컴포넌트 및 matchPath 구현

--- src/core/router/index.js
+++ src/core/router/index.js
@@ -1,4 +1,38 @@
-import { Component } from '../dom';
+import { createElement as h, Component } from '../dom';
+
+export class Link extends Component {
+  render() {
+    return h('a', {
+      href: this.props.to,
+      onClick: this.navigate.bind(this),
+      ...this.props,
+    }, ...this.children);
+  }
+
+  navigate(event) {
+    event.preventDefault();
+    history.pushState({}, null, this.props.to);
+  }
+}
+
+function pathnameToRegExp(pathname) {
+  return pathname
+    .replace(/(?<=\/)\:([a-zA-Z_]+)/, '(?<$1>[^/]+)')
+    .replace(/\//g, '\\/');
+}
+
+function matchPath(pathname, props) {
+  const pathRegex = '^' +
+    pathnameToRegExp(props.path) +
+    (props.exact ? '$' : '');
+  const matched = new RegExp(pathRegex).exec(pathname);
+  if (!matched) return null;
+  return {
+    params: {
+      ...matched.groups
+    }
+  };
+}
 
 export class Router extends Component {
   render() {
@@ -9,14 +43,15 @@ export class Router extends Component {
 export class Switch extends Component {
   render() {
     return this.props.children
-      .find(child => child.props.path === location.pathname) || '';
+      .find(child => matchPath(location.pathname, child.props)) || '';
   }
 }
 
 export class Route extends Component {
   render() {
-    if (this.props.path === location.pathname) {
-      return this.props.children[0];
+    if (matchPath(location.pathname, this.props)) {
+      const child = this.props.children[0];
+      return h(child.type, {match: matchPath(location.pathname, this.props), ...child.props});
     }
     return '';
   }
