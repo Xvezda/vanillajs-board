e8b8935585fcfaebe824b26329ca99099a5ab3ad
Xvezda <xvezda@naver.com>
2022-01-29 07:52:01 +0900

    chore: 빌드시 공유 babel 설정을 사용하도록 수정

--- packages/frontend/webpack.common.js
+++ packages/frontend/webpack.common.js
@@ -18,6 +18,9 @@ module.exports = {
         use: [
           {
             loader: 'babel-loader',
+            options: {
+              rootMode: 'upward'
+            }
           }
         ]
       }
