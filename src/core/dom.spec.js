42b7db0999c5f2aa75a84cdb721673a3118ef3be
Xvezda <xvezda@naver.com>
2022-01-28 09:42:26 +0900

    test: render 테스트 추가

--- src/core/dom.spec.js
+++ src/core/dom.spec.js
@@ -1,5 +1,32 @@
-import { createElement } from './dom';
+/**
+ * @jest-environment jsdom
+ */
+import { createElement as h, render, Component } from './dom';
 
-test('createElement', () => {
-  expect(createElement('div')).toMatchObject({ type: 'div' });
+describe('render', () => {
+  test('hello world 렌더링', () => {
+    const container = document.createElement('div');
+    render(h('h1', null, 'hello world'), container);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test('hello world 컴포넌트 렌더링', () => {
+    class App extends Component {
+      render() {
+        return h('h1', null, 'hello world');
+      }
+    }
+    const container = document.createElement('div');
+    render(h(App), container);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test('변경사항 적용', () => {
+    const container = document.createElement('div');
+    render(h('h1', null, 'hello world'), container);
+    render(h('h1', null, 'foobar'), container);
+    expect(container.innerHTML).toMatch(/foobar/);
+  });
 });
